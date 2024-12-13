2d5fc627a5a3fb1476c1bad27c3792977a85da3e
eddiemuhoro <eddiemuhoro@gmail.com>
2024-12-13 08:24:14 +0300

    Add splash screen, new components, and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Table from "./components/Table";
 import { Route, Routes } from "react-router-dom";
 import DetailsPage from "./components/sales_rows/details/Details";
+import SplashScreen from "./components/splashscreen/SplashScreen";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [showSplash, setShowSplash] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return <SplashScreen />;
+  }
 
   return (
     <>
